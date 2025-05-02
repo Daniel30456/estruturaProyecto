29ef2ed3fd5f1fda4b9a7f5dccb6f5093dada641
Daniel30456 <delgadillodanielfelipe@gmail.com>
2025-05-01 20:16:32 -0500

    Add files via upload

---
+++ juegoestructuraback/src/test/java/co/edu/unbosque/juegoestructuraback/JuegoestructurabackApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.juegoestructuraback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuegoestructurabackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
